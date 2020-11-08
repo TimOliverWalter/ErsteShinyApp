080702a3404b25843df689c0c1de881b2894f4d6
TimOliverWalter <timwalter94@web.de>
2020-11-08 15:29:50 +0100

    Plotly Plots hinzugefügt

--- FirstApp/app.R
+++ FirstApp/app.R
@@ -1,49 +1,41 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinythemes)
+library(plotly)
+library(datasets)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
+ui <- fluidPage(theme = shinytheme("superhero"),
+                navbarPage("Meine erste App",
+                           tabPanel("Iris Datensatz",
+                                    sidebarPanel(selectInput("var_x","X-Variable:",
+                                                             c("Sepal Length"="Sepal.Length",
+                                                               "Petal Length"="Petal.Length")
+                                    )),
+                                    mainPanel(plotlyOutput("iris_data")
+                                              )
+                                    ),
+                           tabPanel("Mtcars Datensatz",
+                                    mainPanel(plotlyOutput("mtcars_data")
+                                              )
+                                    )
+                           )
+                )
+     
+# Define server function  
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$iris_data <- renderPlotly({
+        plot <- plot_ly(x=iris[,input$var_x],
+                        y=iris[,"Petal.Length"],
+                        type="scatter",
+                        mode="markers")
+    })
+    
+    output$mtcars_data <- renderPlotly({
+        plot <- plot_ly(x=mtcars[,"cyl"],
+                        y=mtcars[,"mpg"],
+                        type = "scatter",
+                        mode="markers")
     })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
