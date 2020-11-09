e50bbff3a1df1fb3d25c6eb92038f946162ea69e
TimOliverWalter <timwalter94@web.de>
2020-11-09 15:33:31 +0100

    Spieleralter Range geändert

--- FirstApp/app.R
+++ FirstApp/app.R
@@ -6,7 +6,7 @@ library(htmlwidgets)
 library(dplyr)
 
 fifa19_data <-
-    read.csv("C:/__Meine_Daten/ErsteShinyApp/ErsteShinyApp/data.csv")
+    read.csv("C:/__MeineDaten/ErsteShinyApp/data.csv")
 
 ui <- fluidPage(theme = shinytheme("superhero"),
                 navbarPage(
